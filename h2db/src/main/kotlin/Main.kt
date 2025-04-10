0e31443545167fbc0aa10177bc9fcc48482458c4
jesuuslopeez <jlopezp@iesrafaelalberti.es>
2025-04-10 20:21:38 +0200

    Commit final

---
+++ h2db/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+package org.example
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+
+fun main() {
+   val url = "jdbc:h2:./DataBase/mydb"
+   val usuario = "user"
+   val contrasena = "password"
+   try {
+       Class.forName("org.h2.Driver")
+       val conexion = DriverManager.getConnection(url, usuario, contrasena)
+       println("Conexión exitosa")
+       conexion.close()
+   } catch (e: SQLException) {
+       println("Error en la conexión: ${e.message}")
+   } catch (e: ClassNotFoundException) {
+       println("No se encontró el driver JDBC: ${e.message}")
+   }
+
+}
